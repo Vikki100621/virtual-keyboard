58aba9d8caf70be5b191d345ec21db8eb50a0c28
Viktoriya <kruckovaviktoria421@gmail.com>
2023-05-02 00:38:48 +0600

    fix: added some functions

--- assets/js/index.js
+++ assets/js/index.js
@@ -42,11 +42,24 @@ function createElementsOfkeyboard() {
       const keyElement = createElement('div', 'keyboard-key');
       keyElement.textContent = key;
       keyboardData.find(k => {
-         if (keySymbols[k] === key) {
+
+      if (k =='ShiftLeft' && keySymbols[k] === key) {
+         keyElement.classList.add('ShiftLeft');
+      } if (k =='ShiftRight' && keySymbols[k] === key) {
+         keyElement.classList.add('ShiftRight');
+      } if (k =='AltLeft' && keySymbols[k] === key) {
+         keyElement.classList.add('AltLeft');
+      } if (k =='AltRight' && keySymbols[k] === key) {
+         keyElement.classList.add('AltRight');
+      } if (k =='ControlLeft' && keySymbols[k] === key) {
+         keyElement.classList.add('ControlLeft');
+      }  if (k =='ControlRight' && keySymbols[k] === key) {
+         keyElement.classList.add('ControlRight');
+      } else if (keySymbols[k] === key && k != 'ShiftLeft' && k =='ShiftRight' && k =='AltLeft' && k =='AltRight' && k =='ControlLeft' && k =='ControlRight') {
             const classToAdd = k.toString();
             keyElement.classList.add(classToAdd);
-         }
-      })
+         } 
+      });
       if (key === '⌫' || key === 'Tab↹' || key === 'Enter↵' || key === 'Caps Lock⇪' || key === 'Shift⇧' || key === 'Ctrl' || key === 'Alt' || key === 'Win' || key === ' ') {
          keyElement.classList.add('keyboard-key__width-auto');
       };
@@ -57,26 +70,56 @@ function createElementsOfkeyboard() {
    });
 };
 
-
-
 function highlightKeyOnKeyboard(event) {
    const key = event.code;
-   console.log(key)
    const eventloc = event.location;
    const keyboardKeys = document.querySelectorAll('.keyboard-key');
 
    for (let i = 0; i < keyboardKeys.length; i++) {
       const keyboardKey = keyboardKeys[i];
       const element = keyboardData.find(k => keySymbols[k] === keyboardKey.textContent);
-      if (key === element && event.type === 'keydown' && !keyboardKey.classList.contains('highlight')) {
-         keyboardKey.classList.add('highlight');
-      }
 
+      if (key === element && event.type === 'keydown') {
+         keyboardKey.classList.add('highlight');
+         screen.value += keyboardKey.textContent;
+      } 
+      if (key === 'ArrowUp') {
+         screen.value += '↑';
+       } else if (key === 'ArrowDown') {
+         screen.value += '↓';
+       } else if (key === 'ArrowLeft') {
+         screen.value += '←';
+       } else if (key === 'ArrowRight') {
+         screen.value += '→';
+       }
+       
+       else if (key === 'Enter') {
+         screen.value += '\n';
+       }
+       
+       else if (key === 'Tab') {
+         event.preventDefault();
+         screen.value += '  ';
+       }
+ 
+       else if (key === 'Backspace') {
+         screen.value = screen.value.slice(0, screen.value.length - 1);
+       }
+
+       else if (key === 'Delete') {
+         screen.value = screen.value.slice(0, screen.value.length);
+       }
+ 
+       else if (key === 'Space') {
+         screen.value += ' ';
+       }
+ 
       if (event.type === 'keyup' && keyboardKey.classList.contains('highlight')) {
          keyboardKey.classList.remove('highlight');
       }
    }
 }
-
 document.addEventListener('keydown', highlightKeyOnKeyboard);
 document.addEventListener('keyup', highlightKeyOnKeyboard);
+
+
